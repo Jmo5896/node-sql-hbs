a77f4115bc3d0d3698bd25fdda5a9f670266fe18
Jmo5896 <jmo5896@moore-clan.net>
2019-04-18 20:03:09 -0600

    updated my connection file so that I can use jawsdb

--- connection/connection.js
+++ connection/connection.js
@@ -1,12 +1,20 @@
 'use strict';
 
 let mysql = require('mysql');
-let connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root123',
-  database : 'my_db'
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root123',
+    database : 'my_db'
+  });
+}
+
+
  
 connection.connect((error) => {
     if (error) throw error;
